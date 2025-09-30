d35ee5f9ff357ed459c3a39e9e8ea63aaf0a9687
Bharti <bhartiwadhwa025@gmail.com>
2025-09-30 11:56:14 +0530

    complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
-
 import React from "react";
 import './../styles/App.css';
+import Tooltip from "./Tooltip";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h4>
+      <Tooltip text="This is a tooltip">
+       <span>Hover over me</span>
+       </Tooltip>
+       </h4>
+       <hr></hr>
+         <p>
+      <Tooltip text="This is another tooltip">
+       <span>Hover me to see another tooltip</span>
+       </Tooltip>
+       </p>
     </div>
   )
 }
